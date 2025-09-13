af2e17deaddb99a4e8adcdda33778ba1bf3f2b00
Bianka-lima <bianka.vitoria.silva.lima@ccc.ufcg.edu.br>
2025-09-13 20:12:03 -0300

    feat: isAVL

--- src/main/java/adt/avltree/AVLTreeVerifierImpl.java
+++ src/main/java/adt/avltree/AVLTreeVerifierImpl.java
@@ -1,5 +1,6 @@
 package adt.avltree;
 
+import adt.bst.BSTNode;
 import adt.bst.BSTVerifierImpl;
 
 /**
@@ -25,10 +26,43 @@ public class AVLTreeVerifierImpl<T extends Comparable<T>> extends BSTVerifierImp
 	@Override
 	public boolean isAVLTree() {
 
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		boolean isAVL = true;
 
-		// return isBST() && ...
+		AVLTreeImpl<T> avl = getAVLTree();
+		if (avl != null && !avl.isEmpty()) {
+			isAVL= recursiveIsAVLTree(avl.getRoot());
+		}
+
+		return isBST() && isAVL;
+	}
+
+	private boolean recursiveIsAVLTree(BSTNode<T> node) {
+		boolean isAVL = true;
+		if (node != null && !node.isEmpty()) {
+
+			if (Math.abs(calculateBalance(node)) > 1) {
+				isAVL = false;
+			} else {
+				isAVL = recursiveIsAVLTree((BSTNode<T>)node.getLeft()) && recursiveIsAVLTree((BSTNode<T>)node.getRight());
+			}
+		}
+		return isAVL;
+	}
+
+	protected int calculateBalance(BSTNode<T> node) {
+		int balance = 0;
+		if (node != null && !node.isEmpty()) {
+			balance = recursiveHeight((BSTNode<T>)node.getLeft()) - recursiveHeight((BSTNode<T>)node.getRight());
+		}
+		return balance;
+	}
+
+	public int recursiveHeight(BSTNode<T> node) {
+		int height = -1;
+		if (node != null && !(node.isEmpty())) {
+			height = 1 + Math.max(recursiveHeight((BSTNode<T>)node.getLeft()), recursiveHeight((BSTNode<T>)node.getRight()));
+		}
+		return height;
 	}
 
 }
